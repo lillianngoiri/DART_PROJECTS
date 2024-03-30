a825c1170bfebc3bb2072657d044ea9a803463c3
unknown <lillianngoiri67@gmail.com>
2024-03-30 07:48:20 +0300

    made some changes on the assignment on utilities

--- utilities_assignment.dart
+++ utilities_assignment.dart
@@ -10,26 +10,26 @@ Write a program that uses a try-catch block to catch an exception and output an 
 
 //Create a function that takes two numbers as input and returns the sum of those numbers.
 
-/*   int addNumbers(int num1, int num2) {
+  int addNumbers(int num1, int num2) {
     return num1 + num2;
   }
 
   void main() {
     int add = addNumbers(7, 3);
     print('The sum of the numbers is: $add');
-  } */
+  } 
 
 //Write a program that uses a for loop to print out the numbers from 1 to 10.
-/* 
+
   void main() {
     for (int i = 1; i <= 10; i++) {
       print(i);
     }
-  }  */
+  }  
 
 //Create a program that uses a switch statement to check for different string values and output a response based on the value.
 
-/* void main() {
+void main() {
   String input = 'Lilian';
 
   switch (input) {
@@ -45,29 +45,29 @@ Write a program that uses a try-catch block to catch an exception and output an 
     default:
       print('Your name is Joyce.');
   }
-} */
+} 
 
 //Create a program that uses a while loop to print out the numbers from 20 to 10.
-/*   void main(){
+  void main(){
     int num = 20;
     while(num>=10){
       print (num);
       num--;
     }
-  } */
+  }
 
 //Create a program that uses an if-else statement to check if a number is even or odd and output the result.
-/*    void main(){
+  void main(){
     int num =12;
     if(num%2 ==0){
       print('Number $num is an even number.');
     }else{
       print('Number $num is an odd number.');
     }
-   } */
+   }
 
 //Create a program that takes a list of numbers as input and outputs the largest number in the list.
-/* void main(){
+void main(){
   List<int> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   int max = numbers[0];
   for (int i = 0; i < numbers.length; i++) {
@@ -76,7 +76,7 @@ Write a program that uses a try-catch block to catch an exception and output an 
     }
   }
   print('The largest number in the list is: $max');
-} */
+} 
 
 //Write a program that uses a try-catch block to catch an exception and output an error message.
   void main(){
